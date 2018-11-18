7fa1508b5d15faac9cc4dc0eb923884930c2e93a
Sergey Matyukevich <geomatsi@gmail.com>
2018-11-18 22:51:26 +0300

    update blink-timer-rtfm example
    Update blink-timer-rtfm example according to major
    rtfm update: v0.4.0.
    
    NB: currently rtfm example does not work. We have to wait
    until svd2rust v0.14.0 is released and the device crates
    in the ecosystem catch up before you can run rtfm v0.4.0
    on hardware.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- src/bin/blink-timer-rtfm.rs
+++ src/bin/blink-timer-rtfm.rs
@@ -1,11 +1,13 @@
+//#![feature(extern_crate_item_prelude)]
+//#![feature(custom_attribute)]
 #![no_main]
 #![no_std]
 
-#[macro_use]
-extern crate cortex_m;
+extern crate cortex_m as cm;
+extern crate cortex_m_rt as rt;
 
-extern crate cortex_m_rtfm as rtfm;
-use rtfm::{app, Resource, Threshold};
+extern crate rtfm;
+use rtfm::app;
 
 extern crate panic_itm;
 
@@ -14,84 +16,64 @@ use hal::prelude::*;
 use hal::timer::Event;
 use hal::timer::Timer;
 
-app! {
-    device: hal::stm32f103xx,
+#[app(device = hal::stm32f103xx)]
+const APP: () = {
+    // resources
+    static mut beat: u8 = 0;
+    static mut stim: hal::stm32f103xx::ITM = ();
+    static mut led1: hal::gpio::gpioc::PC13<hal::gpio::Output<hal::gpio::PushPull>> = ();
+    static mut tmr2: hal::timer::Timer<stm32f103xx::TIM2> = ();
+    static mut tmr3: hal::timer::Timer<stm32f103xx::TIM3> = ();
 
-    resources: {
-        static beat: u8 = 0;
-        static stim: hal::stm32f103xx::ITM;
-        static led1: hal::gpio::gpioc::PC13<hal::gpio::Output<hal::gpio::PushPull>>;
-        static tmr2: hal::timer::Timer<stm32f103xx::TIM2>;
-        static tmr3: hal::timer::Timer<stm32f103xx::TIM3>;
-    },
+    #[init]
+    fn init() {
+        let mut rcc = device.RCC.constrain();
 
-    tasks: {
-        TIM2: {
-            priority: 2,
-            path: tim2_handler,
-            resources: [tmr2, beat, stim],
-        },
-        TIM3: {
-            priority: 1,
-            path: tim3_handler,
-            resources: [led1, tmr3, stim],
-        },
+        // configure clocks
+        let mut flash = device.FLASH.constrain();
+        let clocks = rcc
+            .cfgr
+            .sysclk(8.mhz())
+            .pclk1(8.mhz())
+            .freeze(&mut flash.acr);
+
+        // configure PC13 pin to blink LED
+        let mut gpioc = device.GPIOC.split(&mut rcc.apb2);
+        let l1 = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+        // configure and start TIM2 periodic timer
+        let mut t2 = Timer::tim2(device.TIM2, 1.hz(), clocks, &mut rcc.apb1);
+        t2.listen(Event::Update);
+
+        // configure and start TIM3 periodic timer
+        let mut t3 = Timer::tim3(device.TIM3, 5.hz(), clocks, &mut rcc.apb1);
+        t3.listen(Event::Update);
+
+        led1 = l1;
+        tmr2 = t2;
+        tmr3 = t3;
+        stim = core.ITM;
     }
-}
 
-fn init(p: init::Peripherals, _r: init::Resources) -> init::LateResources {
-    let mut rcc = p.device.RCC.constrain();
-
-    // configure clocks
-    let mut flash = p.device.FLASH.constrain();
-    let clocks = rcc
-        .cfgr
-        .sysclk(8.mhz())
-        .pclk1(8.mhz())
-        .freeze(&mut flash.acr);
-
-    // configure PC13 pin to blink LED
-    let mut gpioc = p.device.GPIOC.split(&mut rcc.apb2);
-    let l1 = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-
-    // configure and start TIM2 periodic timer
-    let mut t2 = Timer::tim2(p.device.TIM2, 1.hz(), clocks, &mut rcc.apb1);
-    t2.listen(Event::Update);
-
-    // configure and start TIM3 periodic timer
-    let mut t3 = Timer::tim3(p.device.TIM3, 5.hz(), clocks, &mut rcc.apb1);
-    t3.listen(Event::Update);
-
-    // init late resources
-    init::LateResources {
-        led1: l1,
-        tmr2: t2,
-        tmr3: t3,
-        stim: p.core.ITM,
+    #[idle]
+    fn idle() -> ! {
+        loop {
+            cm::asm::wfi();
+        }
     }
-}
 
-fn idle() -> ! {
-    loop {
-        rtfm::wfi();
+    #[interrupt(resources = [beat, tmr2, stim])]
+    fn TIM2() {
+        let dbg = &mut resources.stim.stim[0];
+        cm::iprintln!(dbg, "TIM2 beat = {}", *resources.beat);
+
+        *resources.beat += 1;
+        resources.tmr2.start(1.hz());
     }
-}
 
-fn tim2_handler(_t: &mut Threshold, mut r: TIM2::Resources) {
-    let dbg = &mut r.stim.stim[0];
-
-    iprintln!(dbg, "TIM2 beat = {}", *r.beat);
-    *r.beat += 1;
-
-    r.tmr2.start(1.hz());
-}
-
-fn tim3_handler(t: &mut Threshold, mut r: TIM3::Resources) {
-    // low priority task needs critical section to access shared resource
-    r.stim.claim_mut(t, |s, _t| {
-        iprintln!(&mut s.stim[0], "blink");
-    });
-
-    r.led1.toggle();
-    r.tmr3.start(5.hz());
-}
+    #[interrupt(resources = [led1, tmr3])]
+    fn TIM3() {
+        resources.led1.toggle();
+        resources.tmr3.start(5.hz());
+    }
+};
