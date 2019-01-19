616346040b6b55737bb77d87df275258181df611
Sergey Matyukevich <geomatsi@gmail.com>
2019-01-20 00:09:23 +0300

    add new example: nRF24 polling Rx example
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- src/bin/nrf24-rtfm-test2.rs
+++ src/bin/nrf24-rtfm-test2.rs
@@ -15,8 +15,6 @@ extern crate stm32f103xx_hal as hal;
 use hal::gpio;
 use hal::prelude::*;
 use hal::spi::Spi;
-use hal::timer::Event;
-use hal::timer::Timer;
 
 extern crate embedded_nrf24l01;
 use embedded_nrf24l01::Configuration;
@@ -40,14 +38,13 @@ type Standby = StandbyMode<
     >,
 >;
 
-// Simple Tx test for embedded-nrf24l01 crate
+// Simple Rx test for embedded-nrf24l01 crate
 
 #[app(device = hal::stm32f103xx)]
 const APP: () = {
     static mut NRF: Option<Standby> = ();
     static mut LED: gpio::gpioc::PC13<hal::gpio::Output<hal::gpio::PushPull>> = ();
     static mut ITM: hal::stm32f103xx::ITM = ();
-    static mut TMR: hal::timer::Timer<stm32f103xx::TIM3> = ();
 
     #[init]
     fn init() {
@@ -97,53 +94,39 @@ const APP: () = {
         nrf.set_rf(DataRate::R250Kbps, 3 /* 0 dBm */).unwrap();
         nrf.set_crc(Some(CrcMode::OneByte)).unwrap();
         nrf.set_auto_retransmit(0b0100, 0b1111).unwrap();
+        nrf.set_auto_ack(&[true; 6]).unwrap();
 
-        // nRF24L01 Tx setup
+        // nRF24L01 Rx setup
         let addr: [u8; 5] = [0xe5, 0xe4, 0xe3, 0xe2, 0xe1];
         nrf.set_rx_addr(0, &addr).unwrap();
-        nrf.set_tx_addr(&addr).unwrap();
         nrf.set_pipes_rx_lengths(&[None; 6]).unwrap();
+        nrf.set_pipes_rx_enable(&[true, false, false, false, false, false])
+            .unwrap();
         nrf.flush_tx().unwrap();
         nrf.flush_rx().unwrap();
-
-        // configure and start TIM3 periodic timer
-        let mut tmr = Timer::tim3(device.TIM3, 1.hz(), clocks, &mut rcc.apb1);
-        tmr.listen(Event::Update);
 
         // init late resources
         NRF = Some(nrf);
         LED = led;
-        TMR = tmr;
         ITM = core.ITM;
     }
 
-    #[idle]
+    #[idle(resources = [LED, ITM, NRF])]
     fn idle() -> ! {
-        loop {
-            cm::asm::wfi();
-        }
-    }
-
-    #[interrupt(resources = [TMR, LED, ITM, NRF])]
-    fn TIM3() {
-        let data = b"hello";
         let dbg = &mut resources.ITM.stim[0];
+        let nrf = resources.NRF.take().expect("NRF is not available");
+        let rx = &mut nrf.rx().unwrap();
 
-        iprintln!(dbg, "TX now");
+        iprintln!(dbg, "NRF Rx is ready to go...");
 
-        if let Some(t) = resources.NRF.take() {
-            let mut t = t.tx().unwrap();
-            t.send(data).unwrap();
+        loop {
+            let pipe = rx.can_read().unwrap();
 
-            let t = t.standby().unwrap();
-            *resources.NRF = Some(t);
-
-            iprintln!(dbg, "TX done");
-        } else {
-            iprintln!(dbg, "NRF busy...");
+            if pipe.is_some() {
+                let data = rx.read().unwrap();
+                iprintln!(dbg, "{:?}", data.as_ref());
+                resources.LED.toggle();
+            }
         }
-
-        resources.LED.toggle();
-        resources.TMR.start(1.hz());
     }
 };
